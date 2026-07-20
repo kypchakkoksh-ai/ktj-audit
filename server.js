b5224bb8052ef589cd0d2152827a818f241d6953
kypchakkoksh-ai <kypchak.koksh@gmail.com>
2026-07-20 18:15:22 +0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { GoogleGenAI } = require('@google/genai');
+const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
